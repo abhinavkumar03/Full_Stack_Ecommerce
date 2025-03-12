c829f3a15cbb7142ebc3ea088db5ee57ac88eedb
Abhinav <abhinav@Admins-MacBook-Air.local>
2025-03-12 19:27:07 +0530

    route reload fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
+import ShopContextProvider from './Context/ShopContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-      <App />
+  <React.StrictMode>
+    <Router>
+      <ShopContextProvider>
+        <App />
+      </ShopContextProvider>
+    </Router>
+  </React.StrictMode>
 );
